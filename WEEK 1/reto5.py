ab609442056178278d789c6354460230ad5d9c9b
Harold <harolduribe28@gmail.com>
2021-05-19 14:57:51 -0500

    Terminé el reto 5

--- WEEK 1/reto5.py
+++ WEEK 1/reto5.py
@@ -5,3 +5,27 @@ Si tu número de comparación se encuentra en el rango de los dos límites, impr
 
 En caso de estar por debajo del inferior o arriba del superior, también muéstralo en pantalla y pide ingresar otro número para repetir el proceso.
 '''
+
+def run():
+    liminf=int(input("Ingresa el límite inferior: "))
+    limsup=int(input("Ingresa el límite superior: "))
+    comparat=int(input('Ingresar el número a comparar: '))
+
+    if liminf>limsup:
+        print('El límite inferior es mayor que el límite superior, por favor ingrese de nuevo los límites.')
+        liminf=int(input("Ingresa el límite inferior: "))
+        limsup=int(input("Ingresa el límite superior: "))
+          
+
+    while True:
+        
+        if comparat<liminf or comparat>limsup:
+            print('El número ', comparat,' está fuera de los límites indicados')
+            comparat=int(input('Ingrese un número dentro de los límites: '))
+            continue
+        else:
+            print('El número ', comparat, ' está en los límites indicados')
+            break
+
+if __name__=='__main__':
+    run()
