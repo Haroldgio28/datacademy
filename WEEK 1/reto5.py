ddcf96df2508359c97f3a56415e42cd2be9d8382
Harold <harolduribe28@gmail.com>
2021-05-19 14:16:12 -0500

    Ahora sí de veras

---
+++ WEEK 1/reto5.py
@@ -0,0 +1,7 @@
+''' Reto 5 - Rangos cambiantes
+Para este reto final juguemos con números. En tu programa pide al usuario ingresar 3 números: un límite inferior, un límite superior y uno de comparación.
+
+Si tu número de comparación se encuentra en el rango de los dos límites, imprímelo en pantalla.
+
+En caso de estar por debajo del inferior o arriba del superior, también muéstralo en pantalla y pide ingresar otro número para repetir el proceso.
+'''
